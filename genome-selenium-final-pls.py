80e715d9f687ef4aec25ba3a1f4d75abbc1fd9c8
Trang Nguyễn <92909404+overthinkingchang@users.noreply.github.com>
2023-02-08 13:51:21 +0700

    Fix bug in adding grch37

--- genome-selenium-final-pls.py
+++ genome-selenium-final-pls.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 
 column_list = ['Chrom', 'Pos', 'ID', 'Ref', 'Alt','KHV', 'KHV-G', 
-               'Region', 'Gene', 'Impact','AAChange']  
+               'Region', 'Gene', 'Impact','AAChange', 'Pos_Grch37']  
 df = pd.DataFrame(columns=column_list)
 driver = webdriver.Chrome(r'C:\Users\DELL\Desktop\chromedriver.exe')
 url = 'https://genomes.vn/'
@@ -24,23 +24,30 @@ def check_exists(sequence):
     check_sequence = driver.find_elements(By.XPATH, "//*[@id='result']/div[1]") 
     driver.implicitly_wait(20)
     return len(check_sequence) > 0
+        
 
 def genome_scraping(sequence):
-    # try: #check if the there contains the SNP first
-    if (check_exists(sequence)): #if not found?
+    if (check_exists(sequence)): 
+        driver.find_element(By.XPATH, "//*[@id='main']/div[3]/label[1]/input").click()
+        driver.find_element(By.ID, 'search-btn').click()
+        driver.implicitly_wait(20) 
+        table_body_grch37 = driver.find_element(
+            By.XPATH, "//*[@id='result']/div[1]/table/tbody") 
+        entries_grch37 = table_body_grch37.find_elements(By.TAG_NAME, 'tr')
+        Pos_value_grch37 = entries_grch37[1].find_element(By.TAG_NAME, 'td').text
+        driver.find_element(By.XPATH, "//*[@id='main']/div[3]/label[2]/input").click()
+        driver.find_element(By.ID, 'search-btn').click()
+        driver.implicitly_wait(20)
         table_body = driver.find_element(
             By.XPATH, "//*[@id='result']/div[1]/table/tbody") 
         entries = table_body.find_elements(By.TAG_NAME, 'tr')
-
         Chrom_value = entries[0].find_element(By.TAG_NAME, 'td').text
         Pos_value = entries[1].find_element(By.TAG_NAME, 'td').text
         ID_value = entries[2].find_element(By.TAG_NAME, 'td').text
         Ref_value = entries[3].find_element(By.TAG_NAME, 'td').text
-
         small_table_body = driver.find_element(
             By.XPATH, "//*[@id='result']/div[1]/table/tbody/tr[5]/td/table/tbody") 
         small_entries_row = small_table_body.find_elements(By.TAG_NAME, 'tr')
-        #row_number = small_table_body.find_elements(By.TAG_NAME, 'th')
         for i in range(0,len(small_entries_row)):
             small_entries_index = small_entries_row[i].find_elements(By.TAG_NAME, 'td')
             Alt = small_entries_row[i].find_element(By.TAG_NAME, 'th').text
@@ -51,11 +58,11 @@ def genome_scraping(sequence):
             Impact_value = small_entries_index[4].text
             AAChange_value = small_entries_index[5].text
             df.loc[len(df.index)] = [Chrom_value, Pos_value, ID_value, Ref_value, Alt, KHV_value, KHVG_value,
-                                 Region_value, Gene_value, Impact_value, AAChange_value]
+                                 Region_value, Gene_value, Impact_value, AAChange_value,Pos_value_grch37]
     # except NoSuchElementException:
     else:
         df.loc[len(df.index)] = ['None', 'None', sequence, 'None', 'None', 'None',
-                                 'None', 'None', 'None', 'None', 'None']
+                                 'None', 'None', 'None', 'None', 'None','None']
         
 def main():
     parser = argparse.ArgumentParser()
