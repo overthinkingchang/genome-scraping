aec60f6ed3ae0282c36801dcc345e7116bb25c55
vumna <vu.nguyenanh.mai@genestory.ai>
2023-02-24 17:02:24 +0700

    restructurize

---
+++ constants/constant_variables.py
@@ -0,0 +1,7 @@
+URL = 'https://genomes.vn/'
+CHOME_DRIVER = r'downloads/chromedriver.exe'
+
+COLUMN_LIST = ['Chrom', 'Pos', 'ID', 'Ref', 'Alt', 'KHV', 'KHV-G',
+               'Region', 'Gene', 'Impact', 'AAChange', 'Pos_Grch37']
+
+WAIT_LARGE_TIME = 10
