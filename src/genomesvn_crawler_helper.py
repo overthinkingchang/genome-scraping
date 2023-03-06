7048ebf9a30ea5687cd87fd4527a732bde59315f
Trang Nguyễn <92909404+overthinkingchang@users.noreply.github.com>
2023-03-06 11:17:49 +0700

    Add option on file input and option

--- src/genomesvn_crawler_helper.py
+++ src/genomesvn_crawler_helper.py
@@ -5,5 +5,25 @@ def initialize_parameters():
     parser.add_argument('--start_id', type=int, required=True)
     parser.add_argument('--end_id', type=int, required=True)
     parser.add_argument('--output_path', type=str, required=True)
-    options = parser.parse_args()
-    return options
+    
+    options, unknown_args = parser.parse_known_args()
+    return options, unknown_args
+
+def file_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filepath', type=str, required=True)
+    parser.add_argument('--output_path', type=str, required=True)
+    input_file = parser.parse_args()
+    return input_file
+
+
+def option():
+    operation_choices = ['list', 'loop']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input",
+                    choices = operation_choices,
+                    default = operation_choices[0],
+                    required = True)
+    input_options = parser.parse_args()
+    return input_options 
+
