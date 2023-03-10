3a5d7de9215316ad11b85b22d776fc18219fbff2
Trang Nguyễn <92909404+overthinkingchang@users.noreply.github.com>
2023-03-10 18:16:59 +0700

    Update genomesvn_crawler_helper.py

--- src/genomesvn_crawler_helper.py
+++ src/genomesvn_crawler_helper.py
@@ -2,28 +2,13 @@ import argparse
 
 def initialize_parameters():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--start_id', type=int, required=True)
-    parser.add_argument('--end_id', type=int, required=True)
+    parser.add_argument('--start_id', type=int, required=False)
+    parser.add_argument('--end_id', type=int, required=False)
+    parser.add_argument('--filepath', type=str, required=False)
     parser.add_argument('--output_path', type=str, required=True)
-    
-    options, unknown_args = parser.parse_known_args()
-    return options, unknown_args
-
-def file_input():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--filepath', type=str, required=True)
-    parser.add_argument('--output_path', type=str, required=True)
-    input_file = parser.parse_args()
-    return input_file
+    options = parser.parse_args()
+    return options
 
 
-def option():
-    operation_choices = ['list', 'loop']
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input",
-                    choices = operation_choices,
-                    default = operation_choices[0],
-                    required = True)
-    input_options = parser.parse_args()
-    return input_options 
+
 
